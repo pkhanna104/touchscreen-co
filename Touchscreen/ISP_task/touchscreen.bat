1d882ab4c420145d4f4b0f6872877919cb30107e
preeya <pkhanna104@gmail.com>
2019-09-05 10:14:20 -0700

    minor change

--- Touchscreen/ISP_task/touchscreen.bat
+++ Touchscreen/ISP_task/touchscreen.bat
@@ -1,3 +1,3 @@
 @echo off
-CALL C:\Users\scientist\Anaconda3\Scripts\activate.bat
-python "C:\Users\scientist\Documents\Preeya\Touchscreen\ISP_task\main.py"
+CALL C:\Users\BasalGangulia\Anaconda3\Scripts\activate.bat
+python "C:\Users\BasalGangulia\Documents\Preeya\Touchscreen\ISP_task\main.py"
