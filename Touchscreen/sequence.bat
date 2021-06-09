3b33e8a4aa727d7748c34defa9fae92f8d2b231b
Sandon Griffin <sandon.griffin@ucsf.edu>
2021-06-08 17:54:42 -0700

    Sequence task update

--- Touchscreen/sequence.bat
+++ Touchscreen/sequence.bat
@@ -1,3 +1,3 @@
 @echo off
-CALL C:\Users\stim\Anaconda3\Scripts\activate.bat
-python "C:\Users\stim\Documents\preeya\touchscreen-co\Touchscreen\Sequence\main.py"
+CALL C:\Users\scientist\Anaconda3\Scripts\activate.bat
+python "C:\Users\scientist\Documents\Preeya\Touchscreen\Sequence\main.py"
