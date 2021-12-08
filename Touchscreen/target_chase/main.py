6dd8d6acfb0a2c7bf3ac969efdb82cfd0752fb5f
Sandon Griffin <sandon.griffin@ucsf.edu>
2021-12-08 13:50:17 -0800

    added seq K to TC

--- Touchscreen/target_chase/main.py
+++ Touchscreen/target_chase/main.py
@@ -286,7 +286,7 @@ class COGame(Widget):
         
                 
         # TARGET POSITIONS
-        seq_opts = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'center out', 'button out']
+        seq_opts = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'center out', 'button out']
         self.seq = False
         for i, val in enumerate(task_in['seq']):
             if val:
@@ -367,6 +367,14 @@ class COGame(Widget):
             self.target3_pos_str = 'lower_right'
             self.target4_pos_str = 'lower_left'
             self.target5_pos_str = 'upper_middle'
+            
+        elif self.seq == 'K':
+            seq_preselect = True
+            self.target1_pos_str = 'upper_middle'
+            self.target2_pos_str = 'middle_right'
+            self.target3_pos_str = 'lower_left'
+            self.target4_pos_str = 'upper_left'
+            self.target5_pos_str = 'upper_right'
         
         elif self.seq == 'center out':
             seq_preselect = True
@@ -2044,6 +2052,7 @@ class Manager(ScreenManager):
     is_seqH = BooleanProperty(False)
     is_seqI = BooleanProperty(False)
     is_seqJ = BooleanProperty(False)
+    is_seqK = BooleanProperty(False)
     is_CO = BooleanProperty(False)
     is_BO = BooleanProperty(False)
     try:
@@ -2067,6 +2076,8 @@ class Manager(ScreenManager):
             is_seqI = BooleanProperty(True) 
         elif data_params['seq'] == 'J':
             is_seqJ = BooleanProperty(True) 
+        elif data_params['seq'] == 'K':
+            is_seqK = BooleanProperty(True) 
         elif data_params['seq'] == 'center out':
             is_CO = BooleanProperty(True) 
         elif data_params['seq'] == 'button out': 
